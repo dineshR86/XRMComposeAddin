53fac0ae5cb7bdfd2dc97e96ef4feef043706323
dineshR86 <dinesh_gudapati@outlook.com>
2019-01-22 23:10:10 +0530

    Added the files for the Save Email controller

---
+++ XRMComposeAddinWeb/Models/SaveEmailRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XRMComposeAddinWeb.Models
+{
+    public class SaveEmailRequest
+    {
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string Sender { get; set; }
+        public string To { get; set; }
+        public string InOut { get; set; }
+        public string Category { get; set; }
+        public string CaseID { get; set; }
+        public string RelatedItemListId { get; set; }
+        public string CreatedDateTime { get; set; }
+    }
+}
